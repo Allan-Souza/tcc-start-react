24bdb04a83b3df2d20f38b62a2f47b6093eb063a
Allan Souza <allanzissimo@gmail.com>
2022-07-20 15:12:30 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Login} from './pages/Login/index'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>
+)
