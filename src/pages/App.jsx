66f3b05d35ab97cf5dacdb98df76f71b1152cd90
Allan Souza <allanzissimo@gmail.com>
2022-07-21 13:00:50 -0300

    primeiro commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div></div>
+  )
+}
+
+export default App
