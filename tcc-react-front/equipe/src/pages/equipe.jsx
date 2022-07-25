4a53ec62bc2a5562a2109b8e77f32c61ebfd4ef4
Joyce Almeida <joycealmeida1998@gmail.com>
2022-07-25 10:52:28 -0300

    HTML dentro de funções

---
+++ tcc-react-front/equipe/src/pages/equipe.jsx
@@ -0,0 +1,41 @@
+export default function Equipe(){
+    return(
+    <main class="background-default pt-5">
+      <div class="contato">
+           <title>Contato</title>
+       
+           <h1>Equipe Sirius Development</h1>
+        
+            <div class="integrantes">
+
+            <div id="allan" class="grid-item">
+                <h2>Allan Souza</h2>
+                <img width="200" height="200" alt="imagem_allan" src="../img/Allan.jpg">
+                <br>
+                <a href="https://www.linkedin.com/in/allan-victor-266ab6198/" target="_blank">Linkedin</a>
+                <a href="https://github.com/Allan-Souza" target="_blank">Github</a> 
+            </div>
+            <br>
+            <div id="Joyce" class="grid-item">
+                <h2>Joyce Almeida</h2>
+                <img width="200" height="200" alt="imagem_joyce" src="../img/Joyce.jpg">
+                <br>
+                <a href="https://www.linkedin.com/in/joyce-almeida-ti/" target="_blank">Linkedin</a>
+                <a href="https://github.com/JoyAlmDev" target="_blank">Github</a> 
+            </div>
+
+            <div id="Rodrigo" class="grid-item">
+                <h2>Rodrigo Santana</h2>
+                <img width="200" height="200" alt="imagem_rodrigo" src="../img/Rodrigo.jpg">
+                <br>
+                <a href="https://www.linkedin.com/in/rodrigoaster/" target="_blank">Linkedin</a>
+                <a href="https://github.com/rodrigoaster" target="_blank">Github</a>
+            </div>
+
+        </div>
+        
+    </div>
+        
+   </main>
+    )
+}
